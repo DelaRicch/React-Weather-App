9bfa5f38b3c62e4949f4dcce834802289359925d
Dela Ricch <DelaRicch@gmail.com>
2022-08-22 16:16:44 +0000

    Done styling the app

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -7,7 +7,35 @@ const Weather = () => {
     const api = 'https://api.openweathermap.org/data/2.5/weather?q=Accra&appid=8f471369af86e6cda49e083a11552a63'
     
   return (
-    <section>Weather</section>
+    <section>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Accra</p>
+          </div>
+          <div className="temp">
+            <h1>30°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="precipitation">
+            <p>Precipitation</p>
+            <p className='details'>45°F</p>
+          </div>
+          <div className="humidity">
+            <p>Humidity</p>
+            <p className='details'>20%</p>
+          </div>
+          <div className="wind">
+            <p>Wind</p>
+            <p className='details'>12 MPH</p>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
